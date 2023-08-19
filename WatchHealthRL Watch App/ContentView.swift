954f864a24f593b2556d983516474b09f89cadeb
UpBeingJames <james@upbeing.ai>
2023-08-19 01:08:38 -0600

    Initial Commit

---
+++ WatchHealthRL Watch App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  WatchHealthRL Watch App
+//
+//  Created by James McKinnon on 2023-08-19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WatchHealthRL Watch App/WatchHealthRLApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchHealthRLApp.swift
+//  WatchHealthRL Watch App
+//
+//  Created by James McKinnon on 2023-08-19.
+//
+
+import SwiftUI
+
+@main
+struct WatchHealthRL_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
